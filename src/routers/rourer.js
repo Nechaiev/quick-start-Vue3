f488c0e7a538adbc67015287c35c726bd42f8d7f
Stanislav <stanislav.nechaev@gmail.com>
2023-11-16 17:10:56 +0200

    add router

---
+++ src/routers/rourer.js
@@ -0,0 +1,25 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return {top: 0}
+    }
+  },
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/posts',
+      name: 'EpisodesViewPosts',
+      component: EpisodesViewPosts,
+    },
+  ]
+});
+export default router
